95b42f8e35358b38b99d56d412d267fb0ce16c9f
Katerina Tikhonova <missoctober390@gmail.com>
2021-01-08 19:10:30 +0300

    Create send password page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { DefaultPage } from 'shared/layout/defaultPage';
 import { Login } from './pages/login';
+import { ForgetPassword } from './pages/forgetPassword';
 import { Signup } from './pages/signup';
 
 export const App: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/login" component={Login} />
+      <Route exact path="/forget-password" component={ForgetPassword} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/" component={DefaultPage} />
     </Switch>
