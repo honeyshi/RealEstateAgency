571606c457b9dccf32f0207d5b5ed0c4b2240acc
Katerina Tikhonova <missoctober390@gmail.com>
2021-01-10 20:59:55 +0300

    Create forget password page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,12 +4,14 @@ import { DefaultPage } from 'shared/layout/defaultPage';
 import { Login } from './pages/login';
 import { Signup } from './pages/signup';
 import { ResetPassword } from './pages/resetPassword';
+import { ForgetPassword } from './pages/forgetPassword';
 
 export const App: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
+      <Route exact path="/forget-password" component={ForgetPassword} />
       <Route exact path="/reset-password" component={ResetPassword} />
       <Route exact path="/" component={DefaultPage} />
     </Switch>
