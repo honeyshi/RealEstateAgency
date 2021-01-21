1e9671256aa9a9b58bb107e6876580b0d50421d2
Katerina Tikhonova <missoctober390@gmail.com>
2021-01-21 22:18:51 +0300

    Create select component

--- src/shared/base/icon.tsx
+++ src/shared/base/icon.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import classNames from 'classnames';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faLocationArrow, faArrowAltCircleRight, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import {
+  faLocationArrow,
+  faArrowAltCircleRight,
+  faBars,
+  faTimes,
+  faAngleDown,
+  faAngleUp,
+} from '@fortawesome/free-solid-svg-icons';
 import { faInstagram, faTelegram, faVk } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { propsToSpace, SpaceProps } from './utils/spaceUtil';
 
-library.add(faLocationArrow, faArrowAltCircleRight, faBars, faTimes, faInstagram, faTelegram, faVk);
+library.add(
+  faLocationArrow,
+  faArrowAltCircleRight,
+  faBars,
+  faTimes,
+  faAngleDown,
+  faAngleUp,
+  faInstagram,
+  faTelegram,
+  faVk
+);
 
 export type ImportedIcon =
   | 'location-arrow'
@@ -16,14 +33,17 @@ export type ImportedIcon =
   | 'vk'
   | 'arrow-alt-circle-right'
   | 'bars'
-  | 'times';
+  | 'times'
+  | 'angle-up'
+  | 'angle-down';
 
 interface IconProps extends SpaceProps {
+  className?: string;
   prefix?: 'fas' | 'far' | 'fab';
   name: ImportedIcon;
 }
 
-export const Icon: React.FC<IconProps> = ({ prefix = 'fas', name, ...other }) => {
-  const classes = classNames(propsToSpace(other));
+export const Icon: React.FC<IconProps> = ({ className, prefix = 'fas', name, ...other }) => {
+  const classes = classNames(propsToSpace(other), className);
   return <FontAwesomeIcon icon={[prefix, name]} className={classes} />;
 };
