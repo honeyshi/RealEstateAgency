963f2917275bbdaa48be4b09429a5898ed2ac5f7
Katerina Tikhonova <missoctober390@gmail.com>
2021-02-05 13:17:50 +0300

    Refactor suggestions section

--- src/app/navbarPages/startPage/sections/suggestionsSection.tsx
+++ src/app/navbarPages/startPage/sections/suggestionsSection.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Container, Flexbox, Row, Section, TextField } from 'shared/base';
 import { Suggestion } from 'pageParts/advertisment';
 import { Link } from 'react-router-dom';
 
-const suggestion = (
-  <Suggestion
-    header="3-комнатная квартира 88 м²"
-    address="ул. Большая Покровская д.8 эт. 4/7"
-    price="15 000 Р/месяц"
-    link="/suggestion"
-  />
-);
-
-const suggestionsRow = [suggestion, suggestion, suggestion];
-
 export const SuggestionsSection: React.FC = () => {
+  const suggestionItemComponents = useMemo(() => {
+    const suggestionItems = [];
+    for (var i = 0; i < 3; i++)
+      suggestionItems.push(
+        <Suggestion
+          header="3-комнатная квартира 88 м²"
+          address="ул. Большая Покровская д.8 эт. 4/7"
+          price="15 000 Р/месяц"
+          link="/suggestion"
+          key={`suggestion-${i}`}
+        />
+      );
+    return suggestionItems;
+  }, []);
   return (
     <Section>
       <Container nonFluid>
@@ -22,8 +25,8 @@ export const SuggestionsSection: React.FC = () => {
           Последние предложения
         </TextField>
         <TextField center>Мы предлагаем обширный выбор недвижимости от собственников.</TextField>
-        <Row>{suggestionsRow}</Row>
-        <Row>{suggestionsRow}</Row>
+        <Row>{suggestionItemComponents}</Row>
+        <Row>{suggestionItemComponents}</Row>
         <Flexbox justifyContent="center" pb="5" pt="0" breakpoint="md" space="pt" className="pt-5">
           <Link to="advert" className="rounded-link text-body">
             Показать ещё
