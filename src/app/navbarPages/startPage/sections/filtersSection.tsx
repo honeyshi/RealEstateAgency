0acfc67c2b32397af290893777838a1da5cf6755
Katerina Tikhonova <missoctober390@gmail.com>
2021-03-29 22:31:27 +0300

    Redesign first section on start page

--- src/app/navbarPages/startPage/sections/filtersSection.tsx
+++ src/app/navbarPages/startPage/sections/filtersSection.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Block, Column, Container, Flexbox, Icon, Row, Section, TextField } from 'shared/base';
-import { StartPageFilters } from 'pageParts/filters';
 import { Link } from 'react-router-dom';
+
+import { Block, Column, Container, Flexbox, Image, RemixIcon, Row, Section, TextField } from 'shared/base';
+import { StartPageFilters } from 'pageParts/filters';
+import image from 'icons/startpage.svg';
 
 export const FiltersSection: React.FC = () => {
   return (
     <Section>
       <Container nonFluid>
-        <Link to="#" className="d-md-flex d-none mr-2 text-black-50">
+        <Link to="#" className="d-md-flex d-none mr-2 text-accent">
           Нижний Новгород
           <Block px="2">
-            <Icon name="location-arrow" />
+            <RemixIcon name="map-pin-5" />
           </Block>
         </Link>
         <Row pb="5" alignItems="center">
-          <Column>
+          <Column size={7}>
             <Flexbox justifyContent="center" vertical>
               <TextField tag="h2" my="5">
                 Снимай жильё из первых рук
               </TextField>
               <TextField>
-                Множество объектов недвижимости в долгосрочную аренду от собственников на одном сайте.
+                Множество объектов недвижимости в долгосрочную аренду <br /> от собственников на одном сайте.
               </TextField>
             </Flexbox>
           </Column>
-          <Column>
-            <Block bg="light" style={{ height: '25rem' }} />
+          <Column size={5}>
+            <Image src={image} />
           </Column>
         </Row>
         <StartPageFilters />
